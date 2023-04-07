ef996c183d5bbba1382bf1912890490f71269b40
GGI-ssoong <sheldon.soong@greenfield.com>
2023-04-07 15:55:00 -0400

    added import functionality with separate draw tool

---
+++ js/import.js
@@ -0,0 +1,48 @@
+import * as graph from './graph.js';
+
+//JSON uploader
+var fileInput = document.createElement('input');
+fileInput.setAttribute('type', 'file');
+fileInput.setAttribute('accept', '.json');
+fileInput.id = "fileInput";
+fileInput.onchange = (event) => {
+    const file = event.target.files[0];
+
+    if (file.type !== 'application/json') {
+      alert('File must be in JSON format.');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsText(file);
+  
+    reader.onload = (event) => {
+      processJSON(event.target.result);
+
+    };
+};
+
+//JSON processor
+async function processJSON(fileContents) {
+  const data = JSON.parse(fileContents);
+  console.log(data)
+  graph.drawGraph(data.nodes, data.links)
+}
+
+//Display Controls
+importControls()
+
+async function importControls() {
+  const importContainer = document.getElementById("import");
+  const importButton = document.createElement("input");
+  importButton.setAttribute("type","button");
+  importButton.setAttribute("value","Import");
+  importButton.setAttribute("onclick","upload()");
+  importContainer.append(importButton);
+
+}
+
+window.upload = function upload() {
+  fileInput.click();
+  return false;
+}
